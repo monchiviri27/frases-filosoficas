aadf7b8ffa60a46652e35eff31f92dc8da42989f
Oswaldo Bazurto <monchiviri27@gmail.com>
2021-03-03 11:19:22 +0100

    cambios respondive mas imagenes nuevas

--- app.js
+++ app.js
@@ -4,51 +4,186 @@ const frases = [
     id:1,
     author: "Tales de Mileto",
     img:"./img/talesDeMileto.jpg",
-    frase : "<<El placer supremo es obtener lo que se anhela.>> <<La cosa más difícil es conocernos a nosotros mismos; la más fácil es hablar mal de los demás.>><<La felicidad del cuerpo se funda en la salud; la del entendimiento, en el saber.>><<Todo es animado y todo está lleno de dioses.>><<La belleza no dimana de un cuerpo hermoso, sino de las bellas acciones.>>"
+    frase1: "_El placer supremo es obtener lo que se anhela.",
+    frase2:"_La cosa más difícil es conocernos a nosotros mismos; la más fácil es hablar mal de los demás.",
+    frase3:"_La felicidad del cuerpo se funda en la salud; la del entendimiento, en el saber.",
+    frase4:"_Todo es animado y todo está lleno de dioses.",
+    frase5:"_La belleza no dimana de un cuerpo hermoso, sino de las bellas acciones."
     },
     
     {
     id:2,
     author : "Sócrates",
     img:"./img/socrates.jpg",
-    frase : "<<No puedo enseñar nada a nadie. Solo puedo hacerles pensar.>>\
-    <<Un hombre honesto es siempre un niño>><<El conocimiento empieza en el asombro>><<Una vida que no ha sido examinada no merece ser vivida>><<Habla para que yo pueda conocerte>><<El amigo debe ser como el dinero; antes de necesitarlo, es necesario saber su valor>><<El único conocimiento verdadero es saber que no sabes nada>>",
+    frase1: "_No puedo enseñar nada a nadie. Solo puedo hacerles pensar.",
+    frase2: "_Un hombre honesto es siempre un niño.",
+    frase3: "_El conocimiento empieza en el asombro.",
+    frase4: "_Una vida que no ha sido examinada no merece ser vivida.",
+    frase5: "_Habla para que yo pueda conocerte."
     },
 
     {
     id:3,
     author : "Jean-Paul Sartre",
     img:"./img/Jean_Paul_Sartre.jpg",
-    frase: " No juzgamos a las personas que amamos.",
+    frase1: "_No juzgamos a las personas que amamos.",
+    frase2: "_No hay necesidad de fuego, el infierno son los otros.",
+    frase3: "_El mundo podría existir muy bien sin la literatura, e incluso mejor sin el hombre.",
+    frase4: "_Felicidad no es hacer lo que uno quiere sino querer lo que uno hace.",
+    frase5: "_Basta con que un hombre odie a otro para que el odio vaya corriendo hasta la humanidad entera."
     
     },
         
     {
     id:4,
-    author : "Sócrates",
-    img:"",
-    frase : "No puedo enseñar nada a nadie. Solo puedo hacerles pensar."
+    author : "Francis Bacon",
+    img:"./img/Francis_Bacon.jpg",
+    frase1: "_El conocimiento es poder.",
+    frase2: "_La muerte es el menor de todos los males.",
+    frase3: "_El requisito del éxito es la prontitud en las decisiones.",
+    frase4: "_Una persona que quiere venganza guarda sus heridas abiertas.",
+    frase5: "_La duda: la escuela de la verdad."
     },
 
     {
     id:5,
-    author : "Sócrates",
-    img:"",
-    frase: "No puedo enseñar nada a nadie. Solo puedo hacerles pensar."
+    author : "Karl Marx",
+    img:"./img/Marx.jpg",
+    frase1: "_La peor lucha es la que no se hace.",
+    frase2: "_La manera como se presentan las cosas no es la manera como son; y si las cosas fueran como se presentan la ciencia entera sobraría.",
+    frase3: "_El motor de la historia es la lucha de clases.",
+    frase4: "_La religión es el opio del pueblo.",
+    frase5: "_La desvalorización del mundo humano crece en razón directa de la valorización del mundo de las cosas."
     },
 
     {
     id:6,
-    author: "Sócrates",
-    img:"",        
-    frase: "No puedo enseñar nada a nadie. Solo puedo hacerles pensar."
+    author: "Platón",
+    img:"./img/platon.jpg",        
+    frase1: "_Pensar es el diálogo del alma consigo misma.",
+    frase2: "_Donde reina el amor sobran las leyes.",
+    frase3: "_De noche, especialmente, es hermoso creer en la luz.",
+    frase4: "_La mejor riqueza es contentarse viviendo con poco.",
+    frase5: "_La música es para el alma lo que la gimnasia para el cuerpo."
     },
     
+    {
+    id:7,
+    author: "Buda",
+    img:"./img/buda.jpg",                
+    frase1: "_Cuida el exterior tanto como el interior, porque todo es uno.",
+    frase2: "_La reflexión es el camino hacia la inmortalidad; la falta de reflexión, el camino hacia la muerte.",
+    frase3: "_No lastimes a los demás con lo que te causa dolor a ti mismo.",
+    frase4: "_El dolor es inevitable, pero el sufrimiento es opcional.",
+    frase5: "_No es más rico quien más tiene, sino quien menos necesita."
+    },
+
+    {
+        id:8,
+        author: "Confucio",
+        img:"./img/confucio.jpg",        
+        frase1: "Exígete mucho a ti mismo y espera poco de los demás. Así te ahorrarás disgustos",
+        frase2:"_La vida es muy simple, pero insistimos en hacerla complicada.",
+        frase3: "_Aprender sin reflexionar es malgastar la energía.",
+        frase4: "_Tenemos que estar en paz con nosotros mismos, si no no podemos guiar a otros en la búsqueda de la paz.",
+        frase5: "_Sólo los sabios más excelentes, y los necios más acabados, son incomprensibles.",
+    
+    },
+
+    {
+        id:9,
+        author: "Aristóteles",
+        img:"./img/aristoteles.jpg",        
+        frase1: "_Nuestras convicciones más arraigadas, más indubitables, son las más sospechosas. Ellas constituyen nuestro límite, nuestros confines, nuestra prisión.",
+        frase2:"_Las raíces de la educación son amargas, pero sus frutos son dulces.",
+        frase3: "_La amistad es un alma que habita en dos cuerpos; un corazón que habita en dos almas.",
+        frase4: "_Lo que con mucho trabajo se adquiere, más se ama.",
+        frase5:"_El hombre solitario es una bestia o un dios."
+
+    },
+
+    {
+        id:10,
+        author: "Schopenhauer",
+        img:"./img/schopenhauer.jpg",        
+        frase1: "Pocas veces pensamos en lo que tenemos; pero siempre en lo que nos falta",
+        frase2:"En general, las nueve décimas partes de nuestra felicidad se fundamentan en la salud",
+        frase3:"_Es difícil encontrar la felicidad dentro de uno mismo, pero es imposible encontrarla en otro lugar.",
+        frase4:"_Los dos enemigos de la felicidad humana son el dolor y el aburrimiento.",
+        frase5:"_El joven debe, tempranamente, estar capacitado para soportar estar solo; ya que es una fuente de felicidad y de paz mental."
+    },
+
+    {
+        id:11,
+        author: "Bertrand Russell",
+        img:"./img/bertrandRussell.jpg",        
+        frase1: "Nunca moriría por mis creencias porque podría estar equivocado"
+    },
+    {
+        id:12,
+        author: "Maquiavelo",
+        img:"./img/maquiavelo.jpg",        
+        frase1: "Todo el mundo ve lo que aparentas ser, pocos experimentan lo que realmente eres"
+    },
+    {
+        id:13,
+        author: "Epíteto",
+        img:"./img/epiteto.jpg",        
+        frase1: "No es lo que te ocurre, sino cómo reaccionas lo que importa"
+    },
+    {
+        id:14,
+        author: "Rousseau",
+        img:"./img/rousseau.jpg",        
+        frase1: "La paciencia es amarga, pero su fruto es dulce"
+    },
+    {
+        id:15,
+        author: "San Agustín",
+        img:"./img/sanAgustin.jpg",        
+        frase1: "La medida del amor es amar sin medida"
+    },
+    {
+        id:16,
+        author: "René Descartes",
+        img:"./img/descartes.jpg",        
+        frase1: "Vivir sin filosofar es, propiamente, tener los ojos cerrados, sin tratar de abrirlos jamás"
+    },
+    {
+        id:17,
+        author: "Kant",
+        img:"./img/kant.jpg",        
+        frase1: "_Vemos las cosas, no como son, sino como somos nosotros.",
+        frase2:"_Tan sólo por la educación puede el hombre llegar a ser hombre. El hombre no es más que lo que la educación hace de él."
+    },
+    {
+        id:18,
+        author: "Diógenes El Cínico",
+        img:"./img/diogenes.jpg",        
+        frase1: "_Cuanto más conozco a la gente, más quiero a mi perro"
+    },
+    {
+        id:19,
+        author: "Voltaire",
+        img:"./img/voltaire.jpg",        
+        frase1: "_Quienes creen que el dinero lo hace todo terminan haciendo todo por dinero."
+    },
+    {
+        id:20,
+        author: "John Locke",
+        img:"./img/locke.jpg",        
+        frase1: "_ Frecuentemente hay más que aprender de las preguntas inesperadas de un niño que de los discursos de un hombre."
+    },
+
 ];
 //seleccionamos items
 const img = document.getElementById('author-img');
 const author = document.getElementById('author');
-const frase = document.getElementById('frase');
+const frase1 = document.getElementById('frase1');
+const frase2 = document.getElementById('frase2');
+const frase3 = document.getElementById('frase3');
+const frase4 = document.getElementById('frase4');
+const frase5= document.getElementById('frase5');
 
 //seleccionamos los botones que accionaran algo
 const prevBtn = document.querySelector('.prevBtn');
@@ -68,7 +203,11 @@ function showAutor(){
     const item = frases[currentItem];
     img.src = item.img;
     author.textContent = item.author;
-    frase.textContent = item.frase;
+    frase1.textContent = item.frase1;
+    frase2.textContent = item.frase2;
+    frase3.textContent = item.frase3;
+    frase4.textContent = item.frase4;
+    frase5.textContent = item.frase5;
 }
 
 //mostrar siguiente autor
